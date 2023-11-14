20cd37fa9b7e9fc7a7f858005655fdc2c74155e7
Maxim Steblyna <m.steblyna@omisoft.net>
2023-11-14 12:30:43 +0200

    added code from exercise 5

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # devops-k8s-demo
+t.me/albiorix_ua_bot.
+build: go build -ldflags "-X="github.com/albiorixUA/devops-k8s-demo/cmd.appVersion=v1.0.2
+start:  ./devops-k8s-demo start
+env: TELE_TOKEN
--- cmd/tgbot.go
+++ cmd/tgbot.go
@@ -5,14 +5,21 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
+	telebot "gopkg.in/telebot.v3"
 )
+
+var TeleToken = os.Getenv("TELE_TOKEN")
 
 // tgbotCmd represents the tgbot command
 var tgbotCmd = &cobra.Command{
-	Use:   "tgbot",
-	Short: "A brief description of your command",
+	Use:     "tgbot",
+	Aliases: []string{"start"},
+	Short:   "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -20,7 +27,29 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("test")
+		fmt.Println("tgbot is started", appVersion)
+		tgbot, err := telebot.NewBot(telebot.Settings{
+			URL:    "",
+			Token:  TeleToken,
+			Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+		})
+
+		if err != nil {
+			log.Fatalf("Please ckeck TELE_TOKEN env variable. %s", err)
+			return
+		}
+		tgbot.Handle(telebot.OnText, func(m telebot.Context) error {
+
+			log.Print(m.Message().Payload, m.Text())
+			payload := m.Message().Payload
+			switch payload {
+			case "hello":
+				err = m.Send(fmt.Sprintf("Hello I'm tgBot %s!", appVersion))
+			}
+			return err
+
+		})
+		tgbot.Start()
 
 	},
 }
